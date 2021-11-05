46de9f8dfe0ec0d3c14944ea1a87bea7b6ce4dac
Eric Stern <github@ericstern.com>
2021-11-05 13:37:41 -0400

    Run config builder in a closure to prevent variable leaks (#6)

--- config.php
+++ config.php
@@ -2,26 +2,30 @@
 
 declare(strict_types=1);
 
-$compile = getenv('ENVIRONMENT') !== 'development';
-if ($compile) {
-    $builder = new Firehed\Container\Compiler('.generated/config.php');
-} else {
-    $builder = new Firehed\Container\Builder();
-}
-
-$blocklist = [
-    'config/cli-config.php', // Doctrine ORM
-];
-
-$files = glob('config/*.php');
-assert($files !== false);
-
-foreach ($files as $file) {
-    // Skip blocked file
-    if (in_array($file, $blocklist, true)) {
-        continue;
+// Run configuration inside of a closure to prevent the intermediate variables
+// leaking into the scope of the file including this.
+return (function () {
+    $compile = getenv('ENVIRONMENT') !== 'development';
+    if ($compile) {
+        $builder = new Firehed\Container\Compiler('.generated/config.php');
+    } else {
+        $builder = new Firehed\Container\Builder();
     }
-    $builder->addFile($file);
-}
 
-return $builder->build();
+    $blocklist = [
+        'config/cli-config.php', // Doctrine ORM
+    ];
+
+    $files = glob('config/*.php');
+    assert($files !== false);
+
+    foreach ($files as $file) {
+        // Skip blocked file
+        if (in_array($file, $blocklist, true)) {
+            continue;
+        }
+        $builder->addFile($file);
+    }
+
+    return $builder->build();
+})();
